a859f4214cba1858737b88e71fe1ae0ea56d0ed2
Martial Michel <martialmichel@datamachines.io>
2019-11-08 09:20:47 -0500

    Modified to use CTO with variable checking

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+if [ -z ${CTO+x} ]; then echo "CTO variable not set, unable to continue"; exit 1; fi
+if [ "A${CTO}" == "A" ]; then echo "CTO variable empty, unable to continue"; exit 1; fi 
+
 xhost +local:docker
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-nvidia-docker run -it --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/dmc --ipc host cuda_tensorflow_opencv:${CTO_TAG} /bin/bash
+nvidia-docker run -it --runtime=nvidia --rm -e DISPLAY=$DISPLAY -v $XSOCK:$XSOCK -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -v ${PWD}:/dmc --ipc host ${CTO} /bin/bash
 xhost -local:docker
