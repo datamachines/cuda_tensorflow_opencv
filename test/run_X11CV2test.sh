93c9f6b914dbed623f93aa9f9b36bd24bd926ebe
Martial Michel <martialmichel@datamachines.io>
2021-02-17 00:00:25 -0500

    Fixed mispelling

--- test/run_X11CV2test.sh
+++ test/run_X11CV2test.sh
@@ -13,7 +13,7 @@ do
   echo "############################################################"
   echo "#################### X11 test for ${CONTAINER_ID}"
   
-  echo "########## Test 1: xeyes will display and disppear after 5 seconds"
+  echo "########## Test 1: xeyes will display and disappear after 5 seconds"
   CONTAINER_ID="${CONTAINER_ID}" ../runDocker.sh -N -c timeout -- 5s /usr/bin/xeyes
 
   echo "########## Test 2: OpenCV to display an image which will disappear after 5 seconds (you can also press any key to continue)"
