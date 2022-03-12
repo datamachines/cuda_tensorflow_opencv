ba2565d3a6eaaa77069e221c4f240740c7ff7bdb
Martial Michel <martialmichel@datamachines.io>
2022-03-12 14:22:58 -0500

    Using same version as for main release

--- Jetson/hub_push_helper.sh
+++ Jetson/hub_push_helper.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-tag=`make -f Makefile | grep tag | cut -d ':' -f 2`
-list=`make -f Makefile| grep -E '\-[[:digit:]]' | tr -s ' ' | cut -d ' ' -f 2`
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+tag=`make -f ${SCRIPTPATH}/Makefile | grep tag | cut -d ':' -f 2`
+list=`make -f ${SCRIPTPATH}/Makefile| grep '-' | tr -s ' ' | cut -d ' ' -f 2`
 
 # Make sure to be `docker login`-ed
 
+echo "***** Getting list from Makefile"
+todo=""
 for i in $list;
 do
-  v=`echo $i | sed 's/-/:/2'`
+  v=`echo $i | sed 's/-/:/'`
   t=`echo $tag | sed 's/\s+//'`
 
   cid="datamachines/$v-$t"
+  echo " - $cid"
+  todo="$todo $cid"  
+done
 
+echo ""
+echo "Press Ctl+c within 5 seconds to cancel"
+for i in 5 4 3 2 1; do echo -n "$i "; sleep 1; done; echo ""
+
+for cid in $todo;
+do
   echo ""
-  echo "##### $cid"
+  echo ""
+  echo "***** Pushing: $cid"
+  echo ""
   docker push $cid || exit 1
-  echo ""
-
 done
 
 echo "Done"
