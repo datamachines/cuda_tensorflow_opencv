cd1e44ec183c98417d30aefc63544a9187a204ca
Martial Michel <martialmichel@datamachines.io>
2022-03-12 14:14:29 -0500

    Needed by Dockerfile

---
+++ Jetson/cupti_helper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# /etc/ld.so.conf.d/nvidia.conf point to /usr/local/nvidia which seems to be missing, point to the cuda directory install for libraries
+cd /usr/local
+if [ -e cuda ]; then
+  if [ ! -e nvidia ]; then
+    ln -s cuda nvidia
+  fi
+fi
+
+# CUPTI
+tmp="/usr/local/cuda/extras/CUPTI/lib64"
+
+if [[ -d $tmp ]] ; then 
+  echo $tmp >> /etc/ld.so.conf.d/nvidia-cupti.conf
+  ldconfig
+  echo "***** CUPTI added to LD path"
+fi
